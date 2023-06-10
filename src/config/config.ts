be3b77d5cb2d11a62e8ef294d4d42077384a2068
tnguy080 <tnguy080@odu.edu>
2023-06-10 10:14:20 +0700

    updated config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -21,24 +21,33 @@ Here are two examples:
 
 const unlockables: Unlockable[] = [
   {
-    cid: "bafybeiehgpaip4f7jafzf7imgannx3nnv3ubaiwp6ph56mlyzijpqxi45m", // HTML blog example
+    cid: "bafybeidmdqagx4tmrrtq5xwyh36iiwb4zwmm2ru24nv2qbudt4awljv6hq", // HTML blog example
     nftId: [
-      "0x3d483f631a391a3706446613929d253cfddcb47900a07593c5004c5e3827d9ee",
+      "0x359af09528ea77b7e1fcc0f78f775d110972222950d30cbdb7804163930b2d02",
     ], // You need to own 'GM #1' to unlock this.
+      // SCR - Wanderer Class Scout Ship
   },
   {
-    cid: "bafybeihx5eacyxeydcpvudwxa242rnjhn67femy46gzas5d2djb24ti5mi", // MP4 video example
+    cid: "bafybeien3riturkp3dz5weivdeyvwjn42zb5ih477tosaphsozccaqxmve", // MP4 video example
     nftId: [
-      "0x8aa9d39f44b4b8488d0bbf04ea12bec99ddbe676a1b9a38d853701327437e78c",
+      "0xfded9278d5861accc474f723b92444189b9cd56b1368084d712b6030a8cbc4fc",
     ], // You need to own 'GM #2' to unlock this.
+      // SCR - Navigator - Helmet
   },
-  {
+    {
+    cid: "bafybeien3riturkp3dz5weivdeyvwjn42zb5ih477tosaphsozccaqxmve", // MP4 video example
+    nftId: [
+      "0x8fa49c7426c2af6ca163938734d3937b015b6daf3e3e4c8df289957bb6dfcf3a",
+    ], // You need to own 'GM #2' to unlock this.
+      // SCR - Marine - Helmet
+  },
+ /* {
     cid: "bafybeihhx5v3saq3b7n55ub5q3atuw2udbqc5ictkv2ih7vd3hxptu22nu", // Web game example
     nftId: [
       "0x8aa9d39f44b4b8488d0bbf04ea12bec99ddbe676a1b9a38d853701327437e78c",
       "0x3d483f631a391a3706446613929d253cfddcb47900a07593c5004c5e3827d9ee",
     ], // You need to own 'GM #1' & 'GM #2' to unlock this.
-  },
+  },*/
 ];
 
 export { unlockables };
